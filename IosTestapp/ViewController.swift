427f096a3969ca0440445c00a23141636b4a1a1a
MaduraDiasTreinetic <102131928+MaduraDiasTreinetic@users.noreply.github.com>
2023-02-28 11:59:26 +0530

    add camera permission handler

--- IosTestapp/ViewController.swift
+++ IosTestapp/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
@@ -16,11 +17,50 @@ class ViewController: UIViewController {
 
     @IBAction func scan_document_btn(_ sender: Any) {
         print("call----->>>>>")
-        let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
-        let vc = storyBoard.instantiateViewController(withIdentifier: "ScanDocumentView") as! ScanDocumentController
-        vc.modalPresentationStyle = .fullScreen
-        self.present(vc, animated: true, completion: nil)
+        
+        
+        if AVCaptureDevice.authorizationStatus(for: AVMediaType.video) ==  AVAuthorizationStatus.authorized
+        {
+        
+            print("Already Authorized----->>>>>")
+            self.navigate()
+            
+           // Already Authorized
+            
+        }
+        else
+        {
+            AVCaptureDevice.requestAccess(for: AVMediaType.video, completionHandler: { (granted :Bool) -> Void in
+              if granted == true
+              {
+                  self.navigate()
+                  
+              }
+              else
+              {
+                 // User Rejected
+                  print("User Rejected----->>>>>")
+           
+              }
+           });
+        }
+        
+        
+      
+ 
     }
+    
+    func navigate(){
+        DispatchQueue.main.async {
+            print("Already Authorized----->>>>>")
+          let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+          let vc = storyBoard.instantiateViewController(withIdentifier: "ScanDocumentView") as! ScanDocumentController
+              vc.modalPresentationStyle = .fullScreen
+              self.present(vc, animated: true, completion: nil)
+        }
+    }
+    
+    
     
 }
 
