4e09b39d48b6f591b798b792dc6179819673f9a5
MaduraDiasTreinetic <102131928+MaduraDiasTreinetic@users.noreply.github.com>
2023-03-03 18:51:21 +0530

    refactored

--- IosTestapp/ScanDocumentController.swift
+++ IosTestapp/ScanDocumentController.swift
@@ -8,12 +8,12 @@
 import UIKit
 import Flutter
 import Foundation
+import FlutterPluginRegistrant
 
-class ScanDocumentController: UIViewController, FlutterAppLifeCycleProvider {
-    
-    private let lifecycleDelegate = FlutterPluginAppLifeCycleDelegate()
-    
-    let flutterEngine = FlutterEngine(name: "flutter_nps_engine")
+class ScanDocumentController: UIViewController{
+    var window: UIWindow?
+    var flutterEngine : FlutterEngine?
+    var isInit = false;
     
     let settings = [
         "token" : "1234",
@@ -21,52 +21,66 @@ class ScanDocumentController: UIViewController, FlutterAppLifeCycleProvider {
         "buttonColor":0xE9E8E8,
         "progressIndicatorColor":0xE9E8E8,
     ] as [String : Any]
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        flutterEngine.run()
-        handleFlutteModule()
+        if(!isInit){
+            // initiate the flutter engine
+            self.flutterEngine = FlutterEngine(name: "io.flutter", project: nil)
+            //start flutter engine
+            self.flutterEngine?.run(withEntrypoint: nil)
+            GeneratedPluginRegistrant.register(with: self.flutterEngine!)
+            isInit = true
+        }
       
     }
     
-    func add(_ delegate: FlutterApplicationLifeCycleDelegate) {
-        lifecycleDelegate.add(delegate)
-    }
+  
     
+    
+    override func viewWillAppear(_ animated: Bool) {
+     //start communicate between flutter and ios
+     handleFlutteModule()
+    }
 
 
     @objc func handleFlutteModule() {
-        let PLATFORM = "com.example.metafy_edge_detection_module";
-        let SEND_SETTINGS_CHANNEL = "sendSettings";
-        let GET_IMAGE_CHANNEL = "getImageUrl";
+        let PLATFORM = "com.example.metafy_edge_detection_module"
+        let SEND_SETTINGS_CHANNEL = "sendSettings"
+        let GET_IMAGE_CHANNEL = "getImageUrl"
+        let CALL_FLUTTER_VIEW_CHANNEL="callFlutterView"
+ 
+     if let flutterEngine = flutterEngine{
+            let channel = FlutterMethodChannel(name: PLATFORM, binaryMessenger: flutterEngine.binaryMessenger)
+         
+         channel.setMethodCallHandler({
+             (call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
+             // send settings to the flutter
+             if(call.method==SEND_SETTINGS_CHANNEL){
+                 result(self.settings)
+       
+             }
+             //start flutter view
+             if(call.method==CALL_FLUTTER_VIEW_CHANNEL){
+                 let flutterViewController = FlutterViewController(engine: flutterEngine, nibName: nil, bundle: nil)
+                 self.present(flutterViewController, animated: true, completion: nil)
+             }
+             // get image url
+             if(call.method==GET_IMAGE_CHANNEL){
+                 let args = call.arguments as? Dictionary<String,Any>
+
+                 let url = args?["url"] as! String
+                 print("arguments from dart side ------->>>>>>>>> ",url)
+                // dismiss the flutterview
+                 self.dismiss(animated: true, completion: nil)
+                 //dismiss the scandocumentController view
+                 self.dismiss(animated: true, completion: nil)
+             }
+
+         })
         
-       let flutterViewController =
-           FlutterViewController(engine: flutterEngine, nibName: nil, bundle: nil)
-
-
-        let channel = FlutterMethodChannel(name: PLATFORM, binaryMessenger: flutterViewController.binaryMessenger)
-
-
-        channel.setMethodCallHandler({
-            (call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
-            if(call.method==SEND_SETTINGS_CHANNEL){
-                result(self.settings)
-
-            }
-            if(call.method==GET_IMAGE_CHANNEL){
-                let args = call.arguments as? Dictionary<String,Any>
-             
-                let url = args?["url"] as! String
-                print("arguments from dart side ------->>>>>>>>> ",url)
-                
-                if let nav = self.navigationController {
-                         nav.popViewController(animated: true)
-                     } else {
-                         self.dismiss(animated: true, completion: nil)
-                
-                     }
-            }
-
-        })
+        }
+        
      }
-
 }
--- IosTestapp/ViewController.swift
+++ IosTestapp/ViewController.swift
@@ -8,59 +8,42 @@
 import UIKit
 import AVFoundation
 
+
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
     }
 
     @IBAction func scan_document_btn(_ sender: Any) {
-        print("call----->>>>>")
-        
-        
         if AVCaptureDevice.authorizationStatus(for: AVMediaType.video) ==  AVAuthorizationStatus.authorized
         {
-        
-            print("Already Authorized----->>>>>")
-            self.navigate()
-            
-           // Already Authorized
-            
+           self.navigate()
         }
         else
         {
             AVCaptureDevice.requestAccess(for: AVMediaType.video, completionHandler: { (granted :Bool) -> Void in
               if granted == true
               {
-                  self.navigate()
-                  
+              self.navigate()
               }
               else
               {
-                 // User Rejected
                   print("User Rejected----->>>>>")
-           
               }
            });
         }
-        
-        
-      
- 
     }
     
     func navigate(){
         DispatchQueue.main.async {
-            print("Already Authorized----->>>>>")
           let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
           let vc = storyBoard.instantiateViewController(withIdentifier: "ScanDocumentView") as! ScanDocumentController
               vc.modalPresentationStyle = .fullScreen
               self.present(vc, animated: true, completion: nil)
         }
     }
-    
-    
-    
 }
 
